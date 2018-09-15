88d8afbac66bb12c79c85de1b4b1d4efa97aa1d2
kaamraankamaal@gmail.com <kaamraan@coviu.com>
2018-09-15 12:41:48 +1000

    start work

--- client/index.js
+++ client/index.js
@@ -1,2 +1,15 @@
-const video = document.getElementById('mainVideo');
-console.log(video);
+const video = document.getElementById('videoEle');
+const canvas = document.getElementById('screenshot');
+const image = document.getElementById('screenshotImg');
+
+navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    console.log('in promise');
+    video.srcObject = stream;
+});
+
+video.onclick = () => {
+    canvas.width = video.width;
+    canvas.height = video.height;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    image.src = canvas.toDataURL('image/webp');
+}
