393b02102f7d9578f5ef654db673e770dcebfa40
kaamraankamaal@gmail.com <kaamraan@coviu.com>
2018-09-15 16:45:43 +1000

    get image from webcam, add live annotations

--- client/index.js
+++ client/index.js
@@ -1,15 +1,48 @@
 const video = document.getElementById('videoEle');
-const canvas = document.getElementById('screenshot');
+const videoCanvas = document.getElementById('video');
+const textCanvas = document.getElementById('text');
 const image = document.getElementById('screenshotImg');
+const btn = document.querySelector('button');
+
+const drawStore = [];
 
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    console.log('in promise');
     video.srcObject = stream;
 });
 
-video.onclick = () => {
-    canvas.width = video.width;
-    canvas.height = video.height;
-    canvas.getContext('2d').drawImage(video, 0, 0);
-    image.src = canvas.toDataURL('image/webp');
+videoCanvas.onclick = () => {
+    image.src = videoCanvas.toDataURL('image/webp');
+}
+
+btn.onclick = () => {
+    // videoCanvas.getContext('2d').strokeRect(10, 10, 80, 90);
+    drawStore.push({
+        x: Math.random() * 640,
+        y: Math.random() * 480,
+        width: Math.random() * 100,
+        height: Math.random() * 100
+    });
+}
+
+video.addEventListener('play', function() {
+    timerCallBack();
+});
+
+function timerCallBack() {
+    if (video.paused || video.ended) {
+        return;
+    }
+    video.style.display = 'none';
+    layOnvideoCanvas();
+    setTimeout(function() {
+        timerCallBack();
+    }, 1000 / 30);
+}
+
+function layOnvideoCanvas() {
+    const context = videoCanvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+    drawStore.forEach(element => {
+        context.strokeRect(element.x, element.y, element.width, element.height);
+    });
 }
