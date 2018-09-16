6626e248793686a7167fedd554a01ed65a084978
Tom Quirk <tomquirkacc@gmail.com>
2018-09-16 14:58:45 +1000

    commit here

--- client/index.js
+++ client/index.js
@@ -1,74 +1,114 @@
-const video = document.getElementById('videoElem');
-const videoCanvas = document.getElementById('video');
-const image = document.getElementById('screenshotImg');
-const btn = document.querySelector('button');
+// Take a screenshot using getUserMedia API.
+// Give credit where credit is due. The code is heavily inspired by
+// HTML5 Rocks' article "Capturing Audio & Video in HTML5"
+// http://www.html5rocks.com/en/tutorials/getusermedia/intro/
+(function() {
+  // Our element ids.
+  var options = {
+    video: "#video",
+    canvas: "#canvas",
+    captureBtn: "#capture-btn",
+    imageURLInput: "#image-url-input"
+  };
 
-const context = videoCanvas.getContext('2d');
-const drawStore = [];
+  // Our object that will hold all of the functions.
+  var App = {
+    // Get the video element.
+    video: document.querySelector(options.video),
+    // Get the canvas element.
+    canvas: document.querySelector(options.canvas),
+    // Get the canvas context.
+    ctx: canvas.getContext("2d"),
+    // Get the capture button.
+    captureBtn: document.querySelector(options.captureBtn),
+    // This will hold the video stream.
+    localMediaStream: null,
+    // This will hold the screenshot base 64 data url.
+    dataURL: null,
+    // This will hold the converted PNG url.
+    imageURL: null,
+    // Get the input field to paste in the imageURL.
+    imageURLInput: document.querySelector(options.imageURLInput),
 
-context.textAlign = 'center';
-context.fillStyle = 'white';
+    initialize: function() {
+      var that = this;
+      // Check if navigator object contains getUserMedia object.
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+      // Check if window contains URL object.
+      window.URL = window.URL || window.webkitURL;
 
-const api = 'http://10.243.125.120:5006/api/predict';
+      // Check for getUserMedia support.
+      if (navigator.getUserMedia) {
+        // Get video stream.
+        navigator.getUserMedia(
+          {
+            video: true
+          },
+          this.gotStream,
+          this.noStream
+        );
 
-navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    video.srcObject = stream;
-});
+        // Bind capture button to capture method.
+        this.captureBtn.onclick = function() {
+          that.capture();
+        };
+      } else {
+        // No getUserMedia support.
+        alert("Your browser does not support getUserMedia API.");
+      }
+    },
 
-videoCanvas.onclick = () => {
-    image.src = videoCanvas.toDataURL('image/jpg');
-}
+    // Stream error.
+    noStream: function(err) {
+      alert("Could not get camera stream.");
+      console.log("Error: ", err);
+    },
 
-btn.onclick = () => {
-    drawStore.push({
-        x: Math.random() * 640,
-        y: Math.random() * 480,
-        width: Math.random() * 100,
-        height: Math.random() * 100
-    });
-}
+    // Stream success.
+    gotStream: function(stream) {
+      video.srcObject = stream;
 
-video.addEventListener('play', function() {
-    layVideoOnCanvasCallback();
-});
+      // Store the stream.
+      localMediaStream = stream;
+    },
 
-function layVideoOnCanvasCallback() {
-    if (video.paused || video.ended) {
-        return;
-    }
-    video.style.display = 'none';
-    layVideoOnCanvas();
-    // add captions
-    context.fillText(captionText, 320, 450);
-    setTimeout(function() {
-        layVideoOnCanvasCallback();
-    }, 1000 / 30); //30 fps
-}
+    // Capture frame from live video stream.
+    capture: function() {
+      var that = this;
+      // Check if has stream.
+      if (localMediaStream) {
+        // Draw whatever is in the video element on to the canvas.
+        that.ctx.drawImage(video, 0, 0);
+        // Create a data url from the canvas image.
+        dataURL = canvas.toDataURL("image/jpeg");
+        // Call our method to save the data url to an image.
+        that.saveDataUrlToImage();
+      }
+    },
 
-function takeScreenshots() {
-    if (video.paused || video.ended) {
-        return;
-    }
-    image.src = videoCanvas.toDataURL('image/webp');
-    setTimeout(function() {
-        takeScreenshots();
-    }, 500);
-}
+    saveDataUrlToImage: function() {
+      var that = this;
 
-function layVideoOnCanvas() {
-    context.drawImage(video, 0, 0);
-    drawStore.forEach(element => {
-        context.strokeRect(element.x, element.y, element.width, element.height);
-    });
-}
-
-function getPrediction(data) {
-    return fetch(api, { method: 'POST', body: videoCanvas.toDataURL('image/jpg') })
-        .then(res => {
-            console.log(res);
-            return res;
+      fetch("http://10.243.125.120:5006/api/predict", {
+        method: "POST",
+        body: dataURL
+      })
+        .then(function(res) {
+          return res.json();
         })
-        .catch(err => {
-            console.log('error', err);
-        })
-}
+        .then(function(data) {
+          alert(JSON.stringify(data));
+        });
+    }
+  };
+
+  // Initialize our application.
+  App.initialize();
+
+  // Expose to window object for testing purposes.
+  window.App = App;
+})();
