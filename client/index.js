d2a1006d2fe8ba447fba315ee2d3d461ee27bda9
kaamraankamaal@gmail.com <kaamraan@coviu.com>
2018-09-16 11:55:38 +1000

    mock captions rectangle

--- client/index.js
+++ client/index.js
@@ -1,10 +1,15 @@
 const video = document.getElementById('videoEle');
 const videoCanvas = document.getElementById('video');
-const textCanvas = document.getElementById('text');
 const image = document.getElementById('screenshotImg');
 const btn = document.querySelector('button');
 
+const context = videoCanvas.getContext('2d');
 const drawStore = [];
+let captionText = '';
+
+context.font = '1.2rem sans-serif';
+context.textAlign = 'center';
+context.fillStyle = 'white';
 
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
     video.srcObject = stream;
@@ -15,7 +20,6 @@ videoCanvas.onclick = () => {
 }
 
 btn.onclick = () => {
-    // videoCanvas.getContext('2d').strokeRect(10, 10, 80, 90);
     drawStore.push({
         x: Math.random() * 640,
         y: Math.random() * 480,
@@ -24,9 +28,24 @@ btn.onclick = () => {
     });
 }
 
+function addCaptionText() {
+    if (video.paused || video.ended) {
+        return;
+    }
+    if (captionText.length >= 5) {
+        captionText = '';
+    }
+    captionText += 'A';
+    setTimeout(function() {
+        addCaptionText();
+    }, 1500);
+
+}
+
 video.addEventListener('play', function() {
     layVideoOnCanvasCallback();
-    startScreenshots();
+    takeScreenshots();
+    addCaptionText();
 });
 
 function layVideoOnCanvasCallback() {
@@ -35,33 +54,25 @@ function layVideoOnCanvasCallback() {
     }
     video.style.display = 'none';
     layVideoOnCanvas();
+    // add captions
+    context.fillText(captionText, 320, 450);
     setTimeout(function() {
         layVideoOnCanvasCallback();
     }, 1000 / 30); //30 fps
 }
 
-function startScreenshots() {
+function takeScreenshots() {
     if (video.paused || video.ended) {
         return;
     }
     image.src = videoCanvas.toDataURL('image/webp');
     setTimeout(function() {
-        startScreenshots();
-    }, 1000);
+        takeScreenshots();
+    }, 500);
 }
 
 function layVideoOnCanvas() {
-    const context = videoCanvas.getContext('2d');
     context.drawImage(video, 0, 0);
-    let frame = context.getImageData(0, 0, videoCanvas.width, videoCanvas.height);
-    for (let i = 0; i < frame.data.length; i++) {
-        let r = frame.data[i * 4 + 0];
-        let g = frame.data[i * 4 + 1];
-        let b = frame.data[i * 4 + 2];
-        if (r - 40 <= 80 && g - 30 <= 50 && b - 40 <= 100)
-            frame.data[i * 4 + 3] = 30;
-    }
-    context.putImageData(frame, 0 ,0);
     drawStore.forEach(element => {
         context.strokeRect(element.x, element.y, element.width, element.height);
     });
