a477e98247cef3ff7cde1240706183ad6104360d
Tom Quirk <tomquirkacc@gmail.com>
2018-09-16 13:13:26 +1000

    lol

--- client/index.js
+++ client/index.js
@@ -1,4 +1,65 @@
-const video = document.getElementById('videoEle');
+// // (() => {
+// const video = document.getElementById("videoElem");
+// const videoCanvas = document.getElementById("video");
+// const image = document.getElementById("screenshotImg");
+// const captionElem = document.querySelector("caption");
+
+// const context = videoCanvas.getContext("2d");
+// const drawStore = [];
+// let captionText = "";
+
+// navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+//   video.srcObject = stream;
+// });
+
+// videoCanvas.onclick = () => {
+//   image.src = videoCanvas.toDataURL("image/webp");
+// };
+
+// const setCaptionText = text => {
+//   captionElem.text = text;
+// };
+
+// video.addEventListener("play", () => {
+//   layVideoOnCanvasCallback();
+
+//   setInterval(() => {
+//     setCaptionText((captionElem.text += "A"));
+//   }, 1000);
+// });
+
+// function layVideoOnCanvasCallback() {
+//   if (video.paused || video.ended) {
+//     return;
+//   }
+//   video.style.display = "none";
+//   layVideoOnCanvas();
+//   // add captions
+//   context.fillText(captionText, 320, 450);
+//   setTimeout(function() {
+//     layVideoOnCanvasCallback();
+//   }, 1000 / 30); //30 fps
+// }
+
+// function takeScreenshots() {
+//   if (video.paused || video.ended) {
+//     return;
+//   }
+
+//   image.src = videoCanvas.toDataURL("image/webp");
+
+//   return;
+// }
+
+// function layVideoOnCanvas() {
+//   context.drawImage(video, 0, 0);
+//   drawStore.forEach(element => {
+//     context.strokeRect(element.x, element.y, element.width, element.height);
+//   });
+// }
+// // })();
+
+const video = document.getElementById('videoElem');
 const videoCanvas = document.getElementById('video');
 const image = document.getElementById('screenshotImg');
 const btn = document.querySelector('button');
@@ -7,7 +68,6 @@ const context = videoCanvas.getContext('2d');
 const drawStore = [];
 let captionText = '';
 
-context.font = '1.2rem sans-serif';
 context.textAlign = 'center';
 context.fillStyle = 'white';
 
